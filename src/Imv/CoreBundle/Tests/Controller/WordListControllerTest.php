1e2effbdd37497fc553db7becfe716fcec803e10
Loick Piera <pyrech@gmail.com>
2014-01-12 20:27:18 +0100

    Update the wordlist controller test

--- src/Imv/CoreBundle/Tests/Controller/WordListControllerTest.php
+++ src/Imv/CoreBundle/Tests/Controller/WordListControllerTest.php
@@ -6,17 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class WordListControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testCrudScenario()
     {
         $client = static::createClient();
 
         // Check index's status code
         $crawler = $client->request('GET', '/list/');
         $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /list/");
-    }
 
-    public function testCrudScenario()
-    {
         $uniqid = uniqid();
         $newName = 'New_'.$uniqid;
         $editName = 'Edit_'.$uniqid;
@@ -24,35 +21,42 @@ class WordListControllerTest extends WebTestCase
         $client = static::createClient();
 
         // Check new request's status code
-        $crawler = $client->request('GET', '/list/new');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /list/new");
+        $crawler = $client->click($crawler->filter('.btn-new')->link());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET new page");
 
         // Fill in the form and submit it
-        $form = $crawler->selectButton('imv_corebundle_wordlist_submit')->form(array(
+        $form = $crawler->filter('.btn-create')->form(array(
             'imv_corebundle_wordlist[name]'  => $newName
         ));
 
         $client->submit($form);
         $crawler = $client->followRedirect();
 
+        // Check show request's status code
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET show page");
+
         // Check data in the show view
         $this->assertEquals(0, $crawler->filter('h1:contains("'.$newName.'")')->count(), 'Missing element h1:contains("'.$newName.'")');
 
-        // Edit the entity
-        $crawler = $client->click($crawler->selectLink('Edit')->link());
+        // Check edit request's status code
+        $crawler = $client->click($crawler->filter('.btn-edit')->link());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET edit page");
 
-        $form = $crawler->selectButton('imv_corebundle_wordlist_submit')->form(array(
+        $form = $crawler->filter('.btn-update')->form(array(
             'imv_corebundle_wordlist[name]'  => $editName
         ));
 
         $client->submit($form);
         $crawler = $client->followRedirect();
 
+        // Check edit request's status code
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET edit page");
+
         // Check the element contains an attribute with value equals to $editName
         $this->assertEquals(1, $crawler->filter('[value="'.$editName.'"]')->count(), 'Missing element [value="'.$editName.'"]');
 
         // Delete the entity
-        $client->submit($crawler->selectButton('form_submit')->form());
+        $client->submit($crawler->filter('.btn-delete')->form());
         $crawler = $client->followRedirect();
 
         // Check the entity has been delete on the list
