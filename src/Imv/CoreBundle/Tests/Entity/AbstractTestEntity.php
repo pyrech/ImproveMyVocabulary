4be8dd6f997354a9535542d134483534fa143dbb
Loick Piera <pyrech@gmail.com>
2014-01-13 13:04:59 +0100

    Add test on WordList entity

---
+++ src/Imv/CoreBundle/Tests/Entity/AbstractTestEntity.php
@@ -0,0 +1,100 @@
+<?php
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
+use Imv\CoreBundle\Entity\EntityInterface;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+abstract class AbstractTestEntity extends WebTestCase
+{
+    /**
+     * @var EntityManager
+     */
+    protected $_em;
+
+    /**
+     * @var string
+     */
+    private $repositoryName;
+
+    /**
+     * @inheritdoc
+     */
+    protected function setUp()
+    {
+        $kernel = static::createKernel();
+        $this->_em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+        $this->_em->beginTransaction();
+        $this->repositoryName = $this->getRepositoryName();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function tearDown()
+    {
+        $this->_em->rollback();
+        parent::tearDown();
+    }
+
+    /**
+     * Return the repository name for the associated entity.
+     *
+     * @return string
+     */
+    abstract protected function getRepositoryName();
+
+    /**
+     * Return a repository for the name given.
+     *
+     * If not repository name was given, it uses the repository of the entity associated
+     *
+     * @param string $repositoryName
+     *
+     * @return EntityRepository
+     */
+    protected function getRepository($repositoryName="")
+    {
+        if(empty($repositoryName)) {
+            $repositoryName = $this->repositoryName;
+        }
+        return $this->_em->getRepository($repositoryName);
+    }
+
+    /**
+     * Find an entity in the database
+     *
+     * @param $id
+     * @param string $repositoryName
+     *
+     * @return null|EntityInterface
+     */
+    protected function findEntity($id, $repositoryName="")
+    {
+        return $this->getRepository($repositoryName)->find($id);
+    }
+
+    /**
+     * Reload an entity from the database
+     *
+     * @param EntityInterface $entity
+     *
+     * @return null|EntityInterface
+     */
+    protected function reload(EntityInterface $entity)
+    {
+        return $this->getRepository()->find($entity->getId());
+    }
+
+    /**
+     * Return a unique string
+     *
+     * @param string $prefix default is 'str_'
+     *
+     * @return string
+     */
+    protected function getUniqueString($prefix='str_')
+    {
+        return uniqid($prefix);
+    }
+} 
---
+++ src/Imv/CoreBundle/Tests/Entity/WordListTest.php
@@ -0,0 +1,129 @@
+<?php
+
+use Imv\CoreBundle\Entity\Word;
+use Imv\CoreBundle\Entity\WordList;
+
+class WordListTest extends AbstractTestEntity
+{
+    /**
+     * @inheritdoc
+     */
+    protected function getRepositoryName()
+    {
+        return 'ImvCoreBundle:WordList';
+    }
+
+    public function testScalarGettersAndSetters()
+    {
+        $listName = $this->getUniqueString();
+
+        $entity = new WordList();
+        $this->_em->persist($entity);
+
+        $entity->setName($listName);
+        $this->_em->flush();
+
+        // Reload the entity
+        $entity = $this->findEntity($entity->getId());
+
+        // Test Name field
+        $this->assertEquals($listName, $entity->getName(), 'Invalid wordlist name');
+    }
+
+    public function testAddWord()
+    {
+        $entity = new WordList();
+
+        $words = array();
+        for($i=0; $i<2; $i++) {
+            $word = new Word();
+            $entity->addWord($word);
+            $words[] = $word;
+        }
+        $this->_em->persist($entity);
+        $this->_em->flush();
+
+        // Reload the entity
+        //$entity = $this->findEntity($entity->getId());
+
+        // Test the number of word associated
+        $this->assertEquals(2, $entity->getCount(), 'Invalid wordlist count');
+        $this->assertEquals(2, $entity->getWords()->count(), 'Invalid number of word');
+    }
+
+    public function testFindWord()
+    {
+        $entity = new WordList();
+
+        $word = new Word();
+        $wordDetails = $this->getUniqueString();
+        $word->setDetails($wordDetails);
+        $entity->addWord($word);
+        $this->_em->flush();
+
+        // Reload the entity
+        //$entity = $this->findEntity($entity->getId());
+
+        // Check a word is findable in a wordlist
+        $found = false;
+        foreach($entity->getWords() as $w) {
+            if ($w->getDetails() == $wordDetails) {
+                $found = true;
+            }
+        }
+        $this->assertTrue($found, 'New word added not found');
+    }
+
+    public function testRemoveWord()
+    {
+        $entity = new WordList();
+
+        $word = new Word();
+        $wordDetails = $this->getUniqueString();
+        $word->setDetails($wordDetails);
+        $entity->addWord($word);
+        $this->_em->flush();
+
+        // Test the number of word associated
+        $this->assertEquals(1, $entity->getCount(), 'Invalid wordlist count');
+
+        $entity->removeWord($word);
+        $this->_em->flush();
+
+        // Reload the entity
+        //$entity = $this->findEntity($entity->getId());
+
+        // Test the number of word associated
+        $this->assertEquals(0, $entity->getCount(), 'Invalid wordlist count');
+        $this->assertEquals(0, $entity->getWords()->count(), 'Invalid number of word');
+
+        // Check a word is not findable in a wordlist after being removed
+        $found = false;
+        foreach($entity->getWords() as $w) {
+            if ($w->getDetails() == $wordDetails) {
+                $found = true;
+            }
+        }
+        $this->assertFalse($found, 'Word removed found');
+    }
+
+    public function testDeleteWord()
+    {
+        $entity = new WordList();
+
+        $word = new Word();
+        $wordDetails = $this->getUniqueString();
+        $word->setDetails($wordDetails);
+        $entity->addWord($word);
+        $this->_em->flush();
+
+        // Test the number of word associated
+        $this->assertEquals(1, $entity->getCount(), 'Invalid wordlist count');
+
+        $this->_em->delete($word);
+        $this->_em->flush();
+
+        // Test the number of word associated
+        $this->assertEquals(0, $entity->getCount(), 'Invalid wordlist count');
+    }
+}
